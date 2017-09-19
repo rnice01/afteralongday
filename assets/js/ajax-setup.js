c4e561b22707b9d4bbee738fc377f2492e5f2197
Rob <rnice4christ@gmail.com>
2017-09-19 17:22:46 +0000

    Fix syntax

--- assets/js/ajax-setup.js
+++ assets/js/ajax-setup.js
@@ -2,7 +2,7 @@ $.ajaxSetup({
     beforeSend: function(xhr, settings) {
         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
             // Only send the token to relative URLs i.e. locally.
-            xhr.setRequestHeader("X-CSRFToken", {% csrf_token %});
+            xhr.setRequestHeader("X-CSRFToken", "{% csrf_token %}");
         }
     }
 });
