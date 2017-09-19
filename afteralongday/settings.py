61abcaad13b47d4a96f9ac427b4dd1cf3f112029
Rob <rnice4christ@gmail.com>
2017-09-19 17:13:48 +0000

    Debugging csrf token not being set for first time users

--- afteralongday/settings.py
+++ afteralongday/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['AFTERALONGDAY_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
